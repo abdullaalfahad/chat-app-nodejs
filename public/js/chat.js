a1e1db0a092fecabb88ba4920d3c62d2d486ac27
fahadevidentbd <fahad@evidentbd.com>
2025-04-28 11:52:09 +0600

    feat: meesages render

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,25 +1,55 @@
 const socket = io();
 
+const messageForm = document.querySelector("#message-form");
+const messageFormInput = messageForm.querySelector("input");
+const messageFormButton = messageForm.querySelector("button");
+const messageFormLocationButton = document.querySelector("#send-location");
+const messages = document.querySelector("#messages");
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationMessageTemplate = document.querySelector("#location-message-template").innerHTML;
+
 socket.on("message", (message) => {
   console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    message,
+  });
+  messages.insertAdjacentHTML("beforeend", html);
 });
 
-document.querySelector("#message-form").addEventListener("submit", (e) => {
+socket.on("locationMessage", (url) => {
+  console.log(url);
+
+  const html = Mustache.render(locationMessageTemplate, {
+    url,
+  });
+  messages.insertAdjacentHTML("beforeend", html);
+});
+
+messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  messageFormButton.setAttribute("disabled", "disabled");
 
   const message = e.target.elements.message.value;
 
   socket.emit("sendMessage", message, (msgs) => {
+    messageFormButton.removeAttribute("disabled");
+    messageFormInput.value = "";
+    messageFormInput.focus();
+
     console.log("Message delivered!");
   });
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+messageFormLocationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser.");
   }
 
+  messageFormLocationButton.setAttribute("disabled", "disabled");
+
   navigator.geolocation.getCurrentPosition((position) => {
+    messageFormLocationButton.removeAttribute("disabled");
     socket.emit(
       "sendLocation",
       {
